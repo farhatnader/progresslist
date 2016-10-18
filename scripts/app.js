36acaa59b8cbc3b6c07e738ca6628dbb94297d5c
Farhat Nader <farhatmnader@gmail.com>
2016-10-17 23:17:53 -0400

    implemented ng-repeat for multi-item list

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,8 @@
 angular.module("progressListApp", [])
 	.controller('mainCtrl', function($scope) {
-		
+		$scope.todos = [
+			{"name": "wash clothes"},
+			{"name": "do homework"},
+			{"name": "work out"}
+		]
 	});

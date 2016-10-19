d9867e6364de0672dbd3ee3f34b9623ec269f8de
Farhat Nader <farhatmnader@gmail.com>
2016-10-19 00:09:11 -0400

    implemented service for data retrieval and manipulation

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,34 @@
 angular.module("progressListApp", [])
-	.controller('mainCtrl', function($scope) {
-		$scope.todos = [
-			{"name": "wash clothes"},
-			{"name": "do homework"},
-			{"name": "work out"}
-		]
+	.controller('mainCtrl', function($scope, dataService) {
+		dataService.getTasks(function(response) {
+			$scope.tasks = response.data;
+		});
+
+		$scope.deleteTask = function(task, index) {
+			dataService.deleteTask(task);
+			$scope.tasks.splice(index, 1);
+		}
+
+		$scope.saveTask = function(task) {
+			dataService.saveTask(task);
+		}
+
+		$scope.addTask = function() {
+			var task = {name: "Edit new task"};
+			$scope.tasks.push(task);
+		}
+	})
+	.service('dataService', function($http) {
+		this.getTasks = function(callback) {
+			$http.get('mock/tasks.json')
+				.then(callback);
+		}
+
+		this.deleteTask = function(task) {
+			console.log("The '" + task.name + "' has been deleted.")
+		}
+
+		this.saveTask = function(task) {
+			console.log("The '" + task.name + "' has been saved.")
+		}
 	});

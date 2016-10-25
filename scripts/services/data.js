1d0db9452b38e5982f7d95003ff5443448bed396
Farhat Nader <farhatmnader@gmail.com>
2016-10-24 22:57:16 -0400

    removed console prints

--- scripts/services/data.js
+++ scripts/services/data.js
@@ -1,34 +1,23 @@
 'use strict';
 
 myModule.service('dataService', function($cookies) {
-	// this.getTasks = function(callback) {
-	// 	$http.get('mock/tasks.json')
-	// 		.then(callback);
-	// }
-
 	this.getStoredTasks = function() {
 		var taskList = $cookies.getAll();
-		console.log(taskList);
-
 		var taskArray = []
-
 		for (var task in taskList) {
 			if (taskList.hasOwnProperty(task)) {
 				var taskObj = $cookies.getObject(task);
 				taskArray.push(taskObj);
 			}
 		}
-
 		return taskArray;
 	}
 
 	this.deleteTask = function(task) {
-		console.log("The '" + task.name + "' has been deleted.")
 		$cookies.remove(task.id)
 	}
 
 	function saveTask(taskObj) {
-		console.log("The task '" + taskObj.name + "' has been saved");
 		$cookies.putObject(taskObj.id, taskObj);
 	}
 
@@ -37,12 +26,10 @@ myModule.service('dataService', function($cookies) {
 	}
 
 	this.saveTasks = function(tasks) {
-		console.log(tasks);
 		tasks.forEach(function(task) {
-			console.log(task);
 			if (task.edited == true) {
 				saveTask(task);
 			}
-		})
+		});
 	}
 });
